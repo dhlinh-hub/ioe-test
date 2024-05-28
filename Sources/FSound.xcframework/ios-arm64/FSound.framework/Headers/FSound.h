838aa4564f6c8ab837674b819c8575c8afa14d7a
linhdh <doanhoai.linh1123@gmail.com>
2024-05-28 13:55:15 +0700

    sd

---
+++ Sources/FSound.xcframework/ios-arm64/FSound.framework/Headers/FSound.h
@@ -0,0 +1,18 @@
+//
+//  FSound.h
+//  FSound
+//
+//  Created by Ishipo on 11/13/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FSound.
+FOUNDATION_EXPORT double FSoundVersionNumber;
+
+//! Project version string for FSound.
+FOUNDATION_EXPORT const unsigned char FSoundVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FSound/PublicHeader.h>
+
+
